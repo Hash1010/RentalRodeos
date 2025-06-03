7de794de22ca6a80e67f3a3bac9d3026369c4382
PriyaNagarajan31 <priyadharshinin31@gmail.com>
2025-06-03 12:26:48 +0530

    Properties Section

---
+++ Schemas/UsrProperties_Detail/UsrProperties_Detail.js
@@ -0,0 +1,8 @@
+define('UsrProperties_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrProperties',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
