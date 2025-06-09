d01d6bbdefc7eb2fb6766902299da8367799e813
PriyaNagarajan31 <priyadharshinin31@gmail.com>
2025-06-09 15:15:01 +0530

    Properties

--- Schemas/UsrProperties_ListPage/UsrProperties_ListPage.js
+++ Schemas/UsrProperties_ListPage/UsrProperties_ListPage.js
@@ -3,6 +3,57 @@ define("UsrProperties_ListPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**
 		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
 			{
 				"operation": "merge",
+				"name": "MenuItem_ImportFromExcel",
+				"values": {
+					"clicked": {
+						"request": "crt.ImportDataRequest",
+						"params": {
+							"entitySchemaName": "UsrProperties"
+						}
+					}
+				}
+			},
+			{
+				"operation": "remove",
+				"name": "SearchFilter",
+				"properties": [
+					"targetAttributes"
+				]
+			},
+			{
+				"operation": "merge",
+				"name": "SearchFilter",
+				"values": {
+					"_filterOptions": {
+						"expose": [
+							{
+								"attribute": "SearchFilter_Items",
+								"converters": [
+									{
+										"converter": "crt.SearchFilterAttributeConverter",
+										"args": [
+											"Items"
+										]
+									}
+								]
+							}
+						],
+						"from": [
+							"SearchFilter_SearchValue",
+							"SearchFilter_FilteredColumnsGroups"
+						]
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "FolderTree",
+				"values": {
+					"rootSchemaName": "UsrProperties"
+				}
+			},
+			{
+				"operation": "merge",
 				"name": "DataTable",
 				"values": {
 					"columns": [
@@ -24,27 +75,37 @@ define("UsrProperties_ListPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**
 							"caption": "#ResourceString(PDS_CreatedBy)#",
 							"dataValueType": 10
 						},
-					]
-				}
-			},
-			{
-				"operation": "merge",
-				"name": "MenuItem_ImportFromExcel",
-				"values": {
-					"clicked": {
-						"request": "crt.ImportDataRequest",
-						"params": {
-							"entitySchemaName": "UsrProperties"
+						{
+							"id": "b76deb20-7fe2-5b4d-9ce8-f09786f1bc96",
+							"code": "PDS_UsrImage",
+							"caption": "#ResourceString(PDS_UsrImage)#",
+							"dataValueType": 16
+						},
+						{
+							"id": "5e0ce824-3420-9101-dc0c-32b7760b416c",
+							"code": "PDS_UsrIsAvailable",
+							"caption": "#ResourceString(PDS_UsrIsAvailable)#",
+							"dataValueType": 12
+						},
+						{
+							"id": "2ff3faf0-9b47-9ac9-4ee8-765cca089a8e",
+							"code": "PDS_UsrRegion",
+							"caption": "#ResourceString(PDS_UsrRegion)#",
+							"dataValueType": 10
+						},
+						{
+							"id": "58deed68-e66c-5752-90b0-b6a597d5b9f9",
+							"code": "PDS_UsrType",
+							"caption": "#ResourceString(PDS_UsrType)#",
+							"dataValueType": 10
+						},
+						{
+							"id": "99f83b0b-24e8-230a-e316-b82cd97422fe",
+							"code": "PDS_UsrMonthlyRent",
+							"caption": "#ResourceString(PDS_UsrMonthlyRent)#",
+							"dataValueType": 6
 						}
-					}
-				}
-			},
-			{
-				"operation": "merge",
-				"name": "FolderTree",
-				"values": {
-					"sourceSchemaName": "FolderTree",
-					"rootSchemaName": "UsrProperties"
+					]
 				}
 			}
 		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
@@ -72,7 +133,60 @@ define("UsrProperties_ListPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**
 						"modelConfig": {
 							"path": "PDS.CreatedBy"
 						}
+					},
+					"PDS_UsrImage": {
+						"modelConfig": {
+							"path": "PDS.UsrImage"
+						}
+					},
+					"PDS_UsrIsAvailable": {
+						"modelConfig": {
+							"path": "PDS.UsrIsAvailable"
+						}
+					},
+					"PDS_UsrRegion": {
+						"modelConfig": {
+							"path": "PDS.UsrRegion"
+						}
+					},
+					"PDS_UsrType": {
+						"modelConfig": {
+							"path": "PDS.UsrType"
+						}
+					},
+					"PDS_UsrMonthlyRent": {
+						"modelConfig": {
+							"path": "PDS.UsrMonthlyRent"
+						}
 					}
+				}
+			},
+			{
+				"operation": "merge",
+				"path": [
+					"attributes",
+					"Items",
+					"modelConfig"
+				],
+				"values": {
+					"filterAttributes": [
+						{
+							"loadOnChange": true,
+							"name": "FolderTree_active_folder_filter"
+						},
+						{
+							"name": "Items_PredefinedFilter",
+							"loadOnChange": true
+						},
+						{
+							"name": "LookupQuickFilterByTag_Items",
+							"loadOnChange": true
+						},
+						{
+							"name": "SearchFilter_Items",
+							"loadOnChange": true
+						}
+					]
 				}
 			}
 		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
@@ -85,7 +199,33 @@ define("UsrProperties_ListPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**
 					"config"
 				],
 				"values": {
-					"entitySchemaName": "UsrProperties"
+					"entitySchemaName": "UsrProperties",
+					"attributes": {
+						"UsrName": {
+							"path": "UsrName"
+						},
+						"CreatedOn": {
+							"path": "CreatedOn"
+						},
+						"CreatedBy": {
+							"path": "CreatedBy"
+						},
+						"UsrImage": {
+							"path": "UsrImage"
+						},
+						"UsrIsAvailable": {
+							"path": "UsrIsAvailable"
+						},
+						"UsrRegion": {
+							"path": "UsrRegion"
+						},
+						"UsrType": {
+							"path": "UsrType"
+						},
+						"UsrMonthlyRent": {
+							"path": "UsrMonthlyRent"
+						}
+					}
 				}
 			}
 		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
