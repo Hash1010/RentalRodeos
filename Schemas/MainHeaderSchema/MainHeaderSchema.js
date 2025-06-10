f3306c1ee6a51e4adc9cb5713b5ec3d0cce2e83b
Hash1010 <mohamed.hashim@psiog.com>
2025-06-11 01:57:25 +0530

    Opportunity lifecycle
    -Opportunity form page
    - 6 BPs
    -Quotation section , form page
    -SQL script to disable OOTB opportunity DCM

--- Schemas/MainHeaderSchema/MainHeaderSchema.js
+++ Schemas/MainHeaderSchema/MainHeaderSchema.js
@@ -1,1 +1,31 @@
- 
+   define("MainHeaderSchema", ["ProcessModuleUtilities"], function(ProcessModuleUtilities) {
+    return {
+        attributes: {},
+        methods: {              
+            init: function() {
+                this.callParent(arguments);
+                this.Terrasoft.ServerChannel.on(Terrasoft.EventName.ON_MESSAGE, this.onPopupMessageReceived, this);
+                   this.GetCurrentUserFieldValue();
+            },
+       
+                     
+
+
+          
+            destroy: function() {
+                this.Terrasoft.ServerChannel.un(Terrasoft.EventName.ON_MESSAGE, this.onPopupMessageReceived, this);
+                this.callParent(arguments);
+            },
+            onPopupMessageReceived: function(sender, message) {
+                if (message && message.Header && message.Header.Sender === "ErrorMessageSender") {
+                    var result = this.Ext.decode(message.Body);
+                    if (result.MessageText && result.Contact === Terrasoft.SysValue.CURRENT_USER_CONTACT.value) {
+                        this.showInformationDialog(result.MessageText);
+                    }
+                }
+			
+            }
+        },
+        diff: []
+    };
+});
---
+++ Schemas/UsrQuoteMiniPage/UsrQuoteMiniPage.js
@@ -0,0 +1,180 @@
+define("UsrQuoteMiniPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "Input_8hfjd6t",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 1,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"multiline": false,
+					"label": "$Resources.Strings.UsrQuoteDS_UsrQuotationCode_e9gxfwm",
+					"labelPosition": "above",
+					"control": "$UsrQuoteDS_UsrQuotationCode_e9gxfwm",
+					"visible": true,
+					"readonly": true,
+					"placeholder": "",
+					"tooltip": ""
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "NumberInput_t99wh8i",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 2,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.PageParameters_UsrIntegerParameter1_tg18hd5",
+					"labelPosition": "above",
+					"control": "$PageParameters_UsrIntegerParameter1_tg18hd5",
+					"visible": true,
+					"readonly": true,
+					"placeholder": "",
+					"tooltip": ""
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "ComboBox_7cgm2sk",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 3,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.PageParameters_UsrLookupParameter1_wllos06",
+					"labelPosition": "above",
+					"control": "$PageParameters_UsrLookupParameter1_wllos06",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": [],
+					"mode": "List",
+					"visible": true,
+					"readonly": true,
+					"placeholder": "",
+					"tooltip": "",
+					"valueDetails": null
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "ComboBox_bwghsdi",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 4,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.PageParameters_UsrLookupParameter1_513quad",
+					"labelPosition": "auto",
+					"control": "$PageParameters_UsrLookupParameter1_513quad",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": [],
+					"visible": false,
+					"readonly": false,
+					"placeholder": "",
+					"tooltip": "",
+					"isSimpleLookup": null,
+					"valueDetails": null,
+					"secondaryDisplayValue": null,
+					"mode": "List"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "addRecord_9h5reso",
+				"values": {
+					"code": "addRecord",
+					"type": "crt.ComboboxSearchTextAction",
+					"icon": "combobox-add-new",
+					"caption": "#ResourceString(addRecord_9h5reso_caption)#",
+					"clicked": {
+						"request": "crt.CreateRecordFromLookupRequest",
+						"params": {}
+					}
+				},
+				"parentName": "ComboBox_bwghsdi",
+				"propertyName": "listActions",
+				"index": 0
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"UsrQuoteDS_UsrQuotationCode_e9gxfwm": {
+						"modelConfig": {
+							"path": "UsrQuoteDS.UsrQuotationCode"
+						}
+					},
+					"PageParameters_UsrLookupParameter1_513quad": {
+						"modelConfig": {
+							"path": "PageParameters.UsrOpportunity"
+						}
+					},
+					"PageParameters_UsrIntegerParameter1_tg18hd5": {
+						"modelConfig": {
+							"path": "PageParameters.UsrAmount"
+						}
+					},
+					"PageParameters_UsrLookupParameter1_wllos06": {
+						"modelConfig": {
+							"path": "PageParameters.UsrProperty"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [],
+				"values": {
+					"dataSources": {
+						"UsrQuoteDS": {
+							"type": "crt.EntityDataSource",
+							"scope": "page",
+							"config": {
+								"entitySchemaName": "UsrQuote",
+								"attributes": {}
+							}
+						}
+					},
+					"primaryDataSourceName": "UsrQuoteDS"
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
